80aa931e07efc7207bad92feb0973c3f7c29264a
David H. R. Clymer <david@zettazebra.com>
2018-09-27 15:58:54 -0400

    Convert to python3 compatible print()

--- spambayes/OptionsClass.py
+++ spambayes/OptionsClass.py
@@ -48,6 +48,8 @@ To Do:
 
 """
 
+from __future__ import print_function
+
 # This module is part of the spambayes project, which is Copyright 2002-2007
 # The Python Software Foundation and is covered by the Python Software
 # Foundation license.
@@ -220,7 +222,7 @@ class Option(object):
         try:
             r = re.compile(self.allowed_values)
         except:
-            print >> sys.stderr, self.allowed_values
+            print(self.allowed_values, file=sys.stderr)
             raise
         s = str(value)
         i = 0
@@ -446,8 +448,8 @@ class OptionsClass(object):
             # doesn't exist, so create it - all the changed options will
             # be added to it
             if self.verbose:
-                print >> sys.stderr, "Creating new configuration file",
-                print >> sys.stderr, filename
+                print("Creating new configuration file", file=sys.stderr)
+                print(filename, file=sys.stderr)
             f = file(filename, "w")
             f.close()
             f = file(filename, "r")
@@ -609,10 +611,11 @@ class OptionsClass(object):
                             self.convert_and_set(section, option, value)
                             self._report_deprecated_error(section, opt)
                         else:
-                            print >> sys.stderr, (
+                            print((
                                 "warning: Invalid option %s in"
-                                " section %s in file %s" %
-                                (opt, sect, filename))
+                                " section %s in file %s") %
+                                (opt, sect, filename),
+                                file=sys.stderr)
                 else:
                     self.convert_and_set(section, option, value)
 
@@ -690,9 +693,10 @@ class OptionsClass(object):
         if self.is_valid(sect, opt, val):
             self._options[sect, opt.lower()].set(val)
         else:
-            print >> sys.stderr, ("Attempted to set [%s] %s with "
-                                  "invalid value %s (%s)" %
-                                  (sect, opt.lower(), val, type(val)))
+            print(("Attempted to set [%s] %s with "
+                   "invalid value %s (%s)" %
+                    (sect, opt.lower(), val, type(val))),
+                    file=sys.stderr)
 
     def set_from_cmdline(self, arg, stream=None):
         """Set option from colon-separated sect:opt:val string.
@@ -713,9 +717,10 @@ class OptionsClass(object):
             self.set(sect, opt, val)
 
     def _report_deprecated_error(self, sect, opt):
-        print >> sys.stderr, (
+        print((
             "Warning: option %s in section %s is deprecated" %
-            (opt, sect))
+            (opt, sect)),
+            file=sys.stderr)
 
     def _report_option_error(self, sect, opt, val, stream, msg):
         if sect in self.sections():
@@ -723,21 +728,21 @@ class OptionsClass(object):
             vopts = [v.split(']', 1)[1] for v in vopts
                        if v.startswith('[%s]'%sect)]
             if opt not in vopts:
-                print >> stream, "Invalid option:", opt
-                print >> stream, "Valid options for", sect, "are:"
+                print("Invalid option: %s" % opt, file=stream)
+                print("Valid options for %s are: %s" % sect, file=stream)
                 vopts = ', '.join(vopts)
                 vopts = wrap(vopts)
                 for line in vopts:
-                    print >> stream, '  ', line
+                    print('  %s' % line, file=stream)
             else:
-                print >> stream, "Invalid value:", msg
+                print("Invalid value: %s" % msg, file=stream)
         else:
-            print >> stream, "Invalid section:", sect
-            print >> stream, "Valid sections are:"
+            print("Invalid section: %s" % sect, file=stream)
+            print("Valid sections are:", file=stream)
             vsects = ', '.join(self.sections())
             vsects = wrap(vsects)
             for line in vsects:
-                print >> stream, '  ', line
+                print('  %s' % line, file=stream)
 
     def __setitem__(self, key, value):
         self.set(key[0], key[1], value)
